3969a417a6b238cc4fde3e8af768e4cce1e2e9aa
mo3789530 <mo3789530@gmail.com>
2021-05-31 22:11:54 +0900

    impl upload file

---
+++ go.mod
@@ -0,0 +1,17 @@
+module filer
+
+go 1.14
+
+require (
+	github.com/Azure/azure-sdk-for-go v55.0.0+incompatible
+	github.com/Azure/azure-storage-blob-go v0.13.0
+	github.com/Azure/go-autorest/autorest v0.11.18 // indirect
+	github.com/form3tech-oss/jwt-go v3.2.3+incompatible // indirect
+	github.com/gofrs/uuid v4.0.0+incompatible // indirect
+	github.com/google/uuid v1.2.0 // indirect
+	github.com/joho/godotenv v1.3.0
+	go.mongodb.org/mongo-driver v1.5.2
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
+	golang.org/x/net v0.0.0-20210525063256-abc453219eb5 // indirect
+	golang.org/x/sys v0.0.0-20210525143221-35b2ab0089ea // indirect
+)
--- handler.go
+++ handler.go
@@ -1,11 +1,217 @@
 package main
 
 import (
+	"context"
+	"crypto/rand"
+	"errors"
 	"fmt"
+	"io"
 	"log"
+	"mime/multipart"
 	"net/http"
+	"net/url"
 	"os"
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/storage"
+	"github.com/Azure/azure-storage-blob-go/azblob"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/joho/godotenv"
 )
+
+var (
+	database   string
+	collection string
+)
+
+const (
+	// environment variables
+	mongoDBConnectionStringEnvVarName = "MONGODB_CONNECTION_STRING"
+	mongoDBDatabaseEnvVarName         = "MONGODB_DATABASE"
+	mongoDBCollectionEnvVarName       = "MONGODB_COLLECTION"
+	azureStorageAccount               = "AZURE_STORAGE_ACCOUNT"
+	azureStorageAccessKey             = "AZURE_STORAGE_ACCESS_KEY"
+)
+
+// define mongodb collection type
+type File struct {
+	ID      primitive.ObjectID `bson:"_id,omitempty"`
+	LinkUrl string             `bson:"url"`
+	UUID    string             `bson:"uuid"`
+}
+
+func handleErrors(err error) {
+	if err != nil {
+		if serr, ok := err.(azblob.StorageError); ok { // This error is a Service-specific
+			switch serr.ServiceCode() { // Compare serviceCode to ServiceCodeXxx constants
+			case azblob.ServiceCodeContainerAlreadyExists:
+				fmt.Println("Received 409. Container already exists")
+				return
+			}
+		}
+		log.Fatal(err)
+	}
+}
+
+// create random string
+func makeRandomStr(digit uint32) (string, error) {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@!?$&#<>"
+
+	// 乱数を生成
+	b := make([]byte, digit)
+	if _, err := rand.Read(b); err != nil {
+		return "", errors.New("unexpected error...")
+	}
+
+	// letters からランダムに取り出して文字列を生成
+	var result string
+	for _, v := range b {
+		// index が letters の長さに収まるように調整
+		result += string(letters[int(v)%len(letters)])
+	}
+	return result, nil
+}
+
+// connects to MongoDB
+func connect() *mongo.Client {
+	mongoDBConnectionString := os.Getenv(mongoDBConnectionStringEnvVarName)
+	if mongoDBConnectionString == "" {
+		log.Fatal("missing environment variable: ", mongoDBConnectionStringEnvVarName)
+	}
+	database = os.Getenv(mongoDBCollectionEnvVarName)
+	if database == "" {
+		log.Fatal("missing environment variable: ", mongoDBDatabaseEnvVarName)
+	}
+	collection = os.Getenv(mongoDBCollectionEnvVarName)
+	if collection == "" {
+		log.Fatal("missing environment variable: ", mongoDBCollectionEnvVarName)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	clientOptions := options.Client().ApplyURI(mongoDBConnectionString).SetDirect(true)
+	c, err := mongo.NewClient(clientOptions)
+	if err != nil {
+		log.Fatalf("unable to initialize connection %v", err)
+	}
+
+	err = c.Connect(ctx)
+
+	if err != nil {
+		log.Fatalf("unable to initialize connection %v", err)
+	}
+	err = c.Ping(ctx, nil)
+	if err != nil {
+		log.Fatalf("unable to connect %v", err)
+	}
+	return c
+}
+
+// create a saved link and uuid
+func create(url string) {
+	c := connect()
+	ctx := context.Background()
+	defer c.Disconnect(ctx)
+
+	fileLinkCollection := c.Database(database).Collection(collection)
+	pass, err := makeRandomStr(8)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r, err := fileLinkCollection.InsertOne(ctx, File{LinkUrl: url, UUID: pass})
+
+	if err != nil {
+		log.Fatalf("failed to add todo %v", err)
+	}
+	fmt.Println("Added file link", r.InsertedID)
+}
+
+// find save link and uuid
+func find(uuid string) (bson.Raw, error) {
+	c := connect()
+	ctx := context.Background()
+	defer c.Disconnect(ctx)
+
+	fileLinkCollection := c.Database(database).Collection(collection)
+	filter := bson.D{{"uuid", uuid}}
+	var doc bson.Raw
+	findOptions := options.FindOne()
+	err := fileLinkCollection.FindOne(ctx, filter, findOptions).Decode(&doc)
+	if err == mongo.ErrNoDocuments {
+		log.Println("document not found")
+		return nil, nil
+	}
+	if err != nil {
+		log.Fatal("failed to find %v", err)
+		return nil, err
+	}
+	return doc, nil
+}
+
+// create storage client
+func createStorageClient() *storage.Container {
+	storageAccountName := os.Getenv(azureStorageAccount)
+	accessKey := os.Getenv("")
+	client, err := storage.NewBasicClient(storageAccountName, accessKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+	containerName := "filer"
+	blobService := client.GetBlobService()
+	container := blobService.GetContainerReference(containerName)
+
+	return container
+}
+
+// file upload to azure storage
+func upload(fileData multipart.File, fileName string) {
+	ctx := context.Background()
+
+	accountName, accountKey := os.Getenv("AZURE_STORAGE_ACCOUNT"), os.Getenv("AZURE_STORAGE_ACCESS_KEY")
+	credential, err := azblob.NewSharedKeyCredential(accountName, accountKey)
+	if err != nil {
+		log.Fatal("Invalid credentials with error: " + err.Error())
+	}
+	p := azblob.NewPipeline(credential, azblob.PipelineOptions{})
+	containerName := "filer"
+	// From the Azure portal, get your storage account blob service URL endpoint.
+	URL, _ := url.Parse(
+		fmt.Sprintf("https://%s.blob.core.windows.net/%s", accountName, containerName))
+
+	containerURL := azblob.NewContainerURL(*URL, p)
+
+	// Create a file to test the upload and download.
+	fmt.Printf("Creating a dummy file to test the upload and download\n")
+	saveFile, err := os.Create(fileName)
+	handleErrors(err)
+	defer saveFile.Close()
+
+	// ファイルにデータを書き込む
+	_, err = io.Copy(saveFile, fileData)
+	handleErrors(err)
+
+	// Here's how to upload a blob.
+	blobURL := containerURL.NewBlockBlobURL(fileName)
+	file, err := os.Open(fileName)
+	handleErrors(err)
+
+	fmt.Printf("Uploading the file with blob name: %s\n", fileName)
+	_, err = azblob.UploadFileToBlockBlob(ctx, file, blobURL, azblob.UploadToBlockBlobOptions{
+		BlockSize:   4 * 1024 * 1024,
+		Parallelism: 16})
+	handleErrors(err)
+}
+
+// download from azure storage
+func download() {
+
+}
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	message := "This HTTP triggered function executed successfully. Pass a name in the query string for a personalized response.\n"
@@ -16,13 +222,48 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, message)
 }
 
+// Upload to Azure storage
+// Generate uuid password
+// Azure storage link and password save to CosmosDB
+// return password
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	// Get file data
+
+	formFile, formFileHeader, err := r.FormFile("file")
+	handleErrors(err)
+	defer formFile.Close()
+
+	// Get file name from FormData
+	upload(formFile, formFileHeader.Filename)
+
+	message := "This HTTP triggered function executed successfully. Pass a name in the query string for a personalized response.\n"
+	name := r.URL.Query().Get("name")
+	if name != "" {
+		message = fmt.Sprintf("Hello, %s. This HTTP triggered function executed successfully.\n", name)
+	}
+	fmt.Fprint(w, message)
+}
+
+// Validation password
+// Download data from azure storage
+func downloadHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
 	listenAddr := ":8080"
 	if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
 		listenAddr = ":" + val
 	}
+	err := godotenv.Load(fmt.Sprintf(".env.local"))
+	if err != nil {
+		// .env読めなかった場合の処理
+		os.Exit(-1)
+	}
 	http.HandleFunc("/api/HttpExample", helloHandler)
 	http.HandleFunc("/api/HttpTrigger", helloHandler)
+	http.HandleFunc("/api/UploadTrigger", uploadHandler)
+	http.HandleFunc("/api/DownloadTrigger", downloadHandler)
 	log.Printf("About to listen on %s. Go to https://127.0.0.1%s/", listenAddr, listenAddr)
 	log.Fatal(http.ListenAndServe(listenAddr, nil))
 }
